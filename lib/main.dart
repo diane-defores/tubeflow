7179ae9f6426a6fa83be2f066d5951017d6ee887
Diane Defores <35034946+dianedef@users.noreply.github.com>
2026-04-14 19:19:45 +0200

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:developer' as developer;
 
-import 'package:clerk_flutter/clerk_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
